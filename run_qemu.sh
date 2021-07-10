6b044cad97b245fc9117d27ec567d608cb8be138
ebiiim <mail@ebiiim.com>
2021-07-10 19:46:20 +0900

    qemu monitor

--- run_qemu.sh
+++ run_qemu.sh
@@ -29,4 +29,5 @@ rm -rf mnt
 qemu-system-x86_64 \
   -drive if=pflash,file=$HOME/osbook/devenv/OVMF_CODE.fd \
   -drive if=pflash,file=$HOME/osbook/devenv/OVMF_VARS.fd \
-  -hda disk.img
+  -hda disk.img \
+  -monitor stdio
